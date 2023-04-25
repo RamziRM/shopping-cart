e78afad6a6c4cf3f743a9190333a9e281fb542c2
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-25 02:19:13 -0300

    config updates

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FBBF24',
+        secondary: '#7C3AED',
+        danger: '#EF4444',
+        warning: '#F59E0B',
+        info: '#10B981',
+        success: '#22C55E',
+        light: '#F3F4F6',
+        dark: '#1F2937',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
