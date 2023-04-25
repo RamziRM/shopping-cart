126c9fd6b99e5513ed4e8ddadff489230677a42c
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-25 20:43:55 -0300

    homepage setup

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return <div>
+    <h1>Welcome to JustShop</h1>
+    <h4>Shop for your favorite non existing items!!</h4>
+    <button>Shop Now</button>
+  </div>;
+}
+
+export default Home;
