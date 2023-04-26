7c13fe46185fb5bc2fb8dae5f0bf2e26d57a84e0
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-26 16:58:47 -0300

    router update - header + home comp

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Home";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 const RouteSwitch = () => {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
-                <Route exact path="/" element={Home} />
+                <Route exact path="/" element={<Home />} />
             {/* <Route path="/shop" element={Shop} /> */}
             </Routes>
       </BrowserRouter>
