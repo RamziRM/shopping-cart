dfb07e135bf51b1267c036e10c10e92392774448
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-26 16:59:05 -0300

    homepg tailwind styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 const Home = () => {
-  return <div>
-    <h1>Welcome to JustShop</h1>
-    <h4>Shop for your favorite non existing items!!</h4>
-    <button>Shop Now</button>
-  </div>;
-}
+  return (
+    <div className="flex flex-col text-center p-10 font-medium">
+        <h1 className="text-5xl py-3">Welcome to <span className="text-blue-700 font-bold">JustShop</span></h1>
+        <h4 className="text-xl pt-3 pb-5">Shop for your favorite non existing items!!</h4>
+        <div>
+            <button className="text-2xl p-4 bg-blue-500 w-fit text-white rounded-lg">Shop Now</button>
+        </div>
+    </div>
+  )};
 
 export default Home;
