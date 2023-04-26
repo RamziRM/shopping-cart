8393de7178923836cc5d6fdcd9c148b7118cd8a6
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-26 17:53:01 -0300

    shop + about routes added

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
 
 const RouteSwitch = () => {
     return (
@@ -9,7 +11,8 @@ const RouteSwitch = () => {
             <Header />
             <Routes>
                 <Route exact path="/" element={<Home />} />
-            {/* <Route path="/shop" element={Shop} /> */}
+                <Route path="/shop" element={<Shop />} />
+                <Route path="/about" element={<About />} />
             </Routes>
       </BrowserRouter>
     );    
