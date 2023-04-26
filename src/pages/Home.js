87c047309ebfa4147d1ad8a63805c95a08525589
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-26 17:53:42 -0300

    link to shop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="flex flex-col text-center p-10 font-medium">
         <h1 className="text-5xl py-3">Welcome to <span className="text-blue-700 font-bold">JustShop</span></h1>
         <h4 className="text-xl pt-3 pb-5">Shop for your favorite non existing items!!</h4>
-        <div>
+        <div className="py-2">
+          <Link 
+          to="/shop"
+          >
             <button className="text-2xl p-4 bg-blue-500 w-fit text-white rounded-lg">Shop Now</button>
+          </Link>
         </div>
     </div>
   )};
