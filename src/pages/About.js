dbefcbd65f0a140704205432990e97d9e1f34eac
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-27 02:39:44 -0300

    darkMode implemented - useEffect

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const About = () => {
+const About = ({ darkMode}) => {
+    useEffect(() => {
+        document.body.classList.toggle('dark', darkMode);
+      }, [darkMode]);
+    
     return (
-        <div>
-            <h1>About</h1>
+        <div className="h-screen dark:bg-gray-900">
+            <h1 className="text-4xl dark:text-white">About</h1>
         </div>
     );
 }
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Shop = () => {
+const Shop = ({darkMode}) => {
+    useEffect(() => {
+        document.body.classList.toggle('dark', darkMode);
+      }, [darkMode]);
+
     return (
-        <div>
-            <h1>Shop</h1>
+        <div className="h-screen dark:bg-gray-900">
+            <h1 className="text-4xl dark:text-white">Shop</h1>
         </div>
     );
 }
