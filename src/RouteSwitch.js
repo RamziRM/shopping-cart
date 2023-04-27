ab61c58730a0223af962c31d1f835f6405d9357b
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-27 02:37:56 -0300

    dark mode state - main app structure

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -6,13 +6,15 @@ import Shop from "./pages/Shop";
 import About from "./pages/About";
 
 const RouteSwitch = () => {
+    const [darkMode, setDarkMode] = useState(false);
+
     return (
         <BrowserRouter>
-            <Header />
+            <Header darkMode={darkMode} setDarkMode={setDarkMode}/>
             <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="/shop" element={<Shop />} />
-                <Route path="/about" element={<About />} />
+                <Route exact path="/" element={<Home darkMode={darkMode} setDarkMode={setDarkMode}/>} />
+                <Route path="/shop" element={<Shop darkMode={darkMode} setDarkMode={setDarkMode}/>} />
+                <Route path="/about" element={<About darkMode={darkMode} setDarkMode={setDarkMode}/>} />
             </Routes>
       </BrowserRouter>
     );    
