72b8a9b4a54e26c3cdfaa6ccc1a4d5f50dd1faad
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-01 18:40:55 -0300

    cartItems array + cartTotal

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,54 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ItemCard from "../components/ItemCard";
+import Cart from "../components/Cart";
 
-const Shop = ({ darkMode }) => {
+const Shop = ({ darkMode, cartOpen, toggleCart }) => {
   useEffect(() => {
     document.body.classList.toggle("dark", darkMode);
   }, [darkMode]);
+
+  const [cartItems, setCartItems] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+
+  const handleAddToCart = (product) => {
+    setCartItems([...cartItems, product]);
+    setCartTotal(cartTotal + product.price);
+  };
 
   // An array of 12 mock products
   const products = [
     {
       id: 1,
       title: "Product 1",
-      price: 10.99,
+      price: 10,
       imageUrl: "https://picsum.photos/204",
     },
     {
       id: 2,
       title: "Product 2",
-      price: 20.99,
+      price: 22,
       imageUrl: "https://picsum.photos/202",
     },
     {
       id: 3,
       title: "Product 3",
-      price: 20.99,
+      price: 22,
       imageUrl: "https://picsum.photos/207",
     },
     {
       id: 4,
       title: "Product 4",
-      price: 10.99,
+      price: 11,
       imageUrl: "https://picsum.photos/211",
     },
     {
       id: 5,
       title: "Product 5",
-      price: 20.99,
+      price: 17,
       imageUrl: "https://picsum.photos/209",
     },
     {
       id: 6,
       title: "Product 6",
-      price: 20.99,
+      price: 266,
       imageUrl: "https://picsum.photos/222",
     },
   ];
 
   return (
     <div className="h-screen dark:bg-gray-900 p-7">
-      <h1 className="text-2xl dark:text-white">Shop</h1>
       <div className="grid grid-cols-2 gap-6 md:grid-cols-3">
         {products.map((product) => (
           <ItemCard
@@ -56,9 +64,16 @@ const Shop = ({ darkMode }) => {
             title={product.title}
             price={product.price}
             imageUrl={product.imageUrl}
+            handleAddToCart={handleAddToCart}
           />
         ))}
       </div>
+      <Cart
+        cartItems={cartItems}
+        total={cartTotal}
+        cartOpen={cartOpen}
+        toggleCart={toggleCart}
+      />
     </div>
   );
 };
