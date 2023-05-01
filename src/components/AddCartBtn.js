02a9c0c2ca924bbebda14737b92d1ea9d5f10f18
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-01 01:58:05 -0300

    add to cart btn - initial

---
+++ src/components/AddCartBtn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddCartBtn = ({}) => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-xs px-4 rounded">
+      Add to Cart
+    </button>
+  );
+};
+
+export default AddCartBtn;
