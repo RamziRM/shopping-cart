b24072fe2d8767df78394d6d5850d31968380eb3
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-01 13:32:48 -0300

    handle add to cart + quantity btns

--- src/components/AddCartBtn.js
+++ src/components/AddCartBtn.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AddCartBtn = ({}) => {
+const AddCartBtn = () => {
+  const [quantity, setQuantity] = useState(1);
+  const [isAddingToCart, setIsAddingToCart] = useState(false);
+
+  const handleClick = () => {
+    setIsAddingToCart(true);
+  };
+
+  const handleIncrease = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleDecrease = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  if (isAddingToCart) {
+    return (
+      <div className="flex items-center border border-gray-300 rounded">
+        <button className="p-2" onClick={handleDecrease}>
+          -
+        </button>
+        <span className="px-2">{quantity}</span>
+        <button className="p-2" onClick={handleIncrease}>
+          +
+        </button>
+      </div>
+    );
+  }
+
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-xs px-4 rounded">
-      Add to Cart
-    </button>
+    <div className="flex items-center space-x-2">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-xs px-4 rounded h-8"
+        onClick={handleClick}
+      >
+        Add to Cart
+      </button>
+    </div>
   );
 };
 
