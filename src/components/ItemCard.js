9ec499513cb72b55c5292a39cf557a3a25b44e8e
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-01 18:40:33 -0300

    passing props + defining variable

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,7 +1,14 @@
 import React from "react";
 import AddCartBtn from "./AddCartBtn";
 
-const ItemCard = ({ title, price, imageUrl }) => {
+const ItemCard = ({ id, title, price, imageUrl, handleAddToCart }) => {
+  const product = {
+    id,
+    title,
+    price,
+    imageUrl,
+  };
+
   return (
     <div className="bg-slate-100 shadow-lg rounded-lg dark:bg-slate-600 dark:text-white overflow-hidden">
       <img className="w-full" src={imageUrl} alt={title} />
@@ -10,7 +17,7 @@ const ItemCard = ({ title, price, imageUrl }) => {
           <h2 className="font-bold text-sm mb-2">{title}</h2>
           <p className="text-gray-700 dark:text-white text-sm">${price}</p>
         </div>
-        <AddCartBtn />
+        <AddCartBtn handleAddToCart={handleAddToCart} product={product} />
       </div>
     </div>
   );
