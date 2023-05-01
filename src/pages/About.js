d3236c92d800da6d8eb9af111cba90abe5570a2c
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-30 21:04:31 -0300

    prettier

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
 
-const About = ({ darkMode}) => {
-    useEffect(() => {
-        document.body.classList.toggle('dark', darkMode);
-      }, [darkMode]);
-    
-    return (
-        <div className="h-screen dark:bg-gray-900">
-            <h1 className="text-4xl dark:text-white">About</h1>
-        </div>
-    );
-}
+const About = ({ darkMode }) => {
+  useEffect(() => {
+    document.body.classList.toggle("dark", darkMode);
+  }, [darkMode]);
+
+  return (
+    <div className="h-screen dark:bg-gray-900">
+      <h1 className="text-4xl dark:text-white">About</h1>
+    </div>
+  );
+};
 
 export default About;
